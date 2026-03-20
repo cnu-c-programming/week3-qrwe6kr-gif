a2ef2b8d622933d5e5ad0f6b685700b5617054d9
qrwe6kr-gif <qrwe6kr@gmail.com>
2026-03-20 17:41:44 +0900

    Update 3-4.c

---
+++ code/3-4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+int max_of(int count, ...) {
+    va_list ap;
+    va_start(ap, count);
+    
+    int max = va_arg(ap, int);
+    
+    for (int i = 1; i<count; i++) {
+        int x = va_arg(ap, int);
+        if (max < x) {
+            max = x;
+        }
+    }
+    va_end(ap);
+    
+    return (int)max;
+}
+
+int main() {
+    printf("%d\n", max_of(4, 5, 12, 3, 9));
+    printf("%d\n", max_of(6, 1, 2, 3, 4, 5, 6));
+    printf("%d\n", max_of(3, 100, 200, 300));
+    
+    return 0;
+}

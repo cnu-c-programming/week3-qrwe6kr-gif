12dd52a3541bb2b1aeb04c348399e0d411181715
qrwe6kr-gif <qrwe6kr@gmail.com>
2026-03-20 16:12:06 +0900

    Update 1-1.c

---
+++ code/1-1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+static int add(int a, int b) {
+    return a+b;
+}
+static int sub(int a, int b) {
+    return a-b;
+}
+static int mul(int a, int b) {
+    return a*b;
+}
+static int div(int a, int b) {
+    return a/b;
+}
+
+int main(void) {
+    int a = 10;
+    int b = 5;
+    
+    printf("%d + %d = %d\n", a, b, add(a, b));
+    printf("%d - %d = %d\n", a, b, sub(a, b));
+    printf("%d * %d = %d\n", a, b, mul(a, b));
+    printf("%d / %d = %d\n", a, b, div(a, b));
+    
+    
+
+    return 0;
+}

6ca8996e086686b8c84d55c61c3f7188b916771d
qrwe6kr-gif <qrwe6kr@gmail.com>
2026-03-20 16:35:10 +0900

    Update 2-2.c

---
+++ code/2-2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void swap(int *x, int *y) {
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+int main() {
+    int x = 100; 
+    int y = 200;
+    
+    printf("x = %d, y = %d\n", x, y);
+    swap(&x, &y);
+    printf("x = %d, y = %d\n", x, y);
+    
+    return 0;
+}
